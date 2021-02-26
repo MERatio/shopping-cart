62ad7960725c44e411f09f3e80299fbeda3a60f4
Mark Edward Ratio <markedwardratio@gmail.com>
2021-02-26 14:54:14 +0800

    Fetch and set products

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
@@ -6,6 +7,18 @@ import Cart from './components/Cart';
 import './App.css';
 
 function App() {
+	const [products, setProducts] = useState([]);
+
+	async function fetchAndSetProducts() {
+		const response = await fetch('https://fakestoreapi.com/products?limit=20');
+		const products = await response.json();
+		setProducts(products);
+	}
+
+	useEffect(() => {
+		fetchAndSetProducts();
+	}, []);
+
 	return (
 		<Router>
 			<Navbar />
