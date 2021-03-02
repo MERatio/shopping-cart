506c8b22fb8c8ac81b24d5b5a022c2116b946bdd
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-02 12:14:52 +0800

    Add static number of shopping cart items in ShoppingCartLink

--- src/components/ShoppingCartLink.js
+++ src/components/ShoppingCartLink.js
@@ -1,11 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import '../css/ShoppingCartLink.css';
 
 function ShoppingCartLink() {
 	return (
-		<NavLink exact to="/cart" className="nav-link">
+		<NavLink exact to="/cart" className="nav-link ShoppingCartLink">
 			<FontAwesomeIcon icon={faShoppingCart} size="3x" className="carrot" />
+			<span className="shopping-cart-items-number">0</span>
 		</NavLink>
 	);
 }
