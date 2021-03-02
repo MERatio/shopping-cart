bc4f2b26153b7c14c54d095282f77c5b5a8445d1
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-02 12:53:59 +0800

    Add input change handler in AddToCartForm

--- src/components/AddToCartForm.js
+++ src/components/AddToCartForm.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import '../css/AddToCartForm.css';
 
 function AddToCartForm() {
+	const [state, setState] = useState({ quantity: 1 });
+
+	function handleInputChange(e) {
+		e.preventDefault();
+		const target = e.target;
+		const name = target.name;
+		const value = target.value;
+		setState({ [name]: value });
+	}
+
 	return (
 		<form>
 			<div className="form-group d-flex align-items-center">
@@ -11,10 +22,12 @@ function AddToCartForm() {
 					type="number"
 					className="form-control ml-3 AddToCartForm-quantity"
 					id="quantity"
-					value="1"
+					name="quantity"
+					value={state.quantity}
 					min="1"
 					step="1"
 					required
+					onChange={handleInputChange}
 				/>
 			</div>
 			<button className="btn btn-success">Add To Cart</button>
