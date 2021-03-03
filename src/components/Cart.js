ad45ecce042d16efb5bf574b4ffef8278efc7a8e
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-03 13:09:01 +0800

    Make Cart item image and title link to its product page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import QuantityGroup from './QuantityGroup';
@@ -13,14 +14,20 @@ function Cart({ mappedCartItems, onQuantityInputChange, onCartItemDelete }) {
 						className="row align-items-center text-center mb-4"
 					>
 						<div className="col-md-3 mb-2">
-							<img
-								src={mappedCartItem.product.image}
-								alt={mappedCartItem.product.title}
-								className="Cart-product-img"
-							/>
+							<Link to={`/products/${mappedCartItem.product.id}`}>
+								<img
+									src={mappedCartItem.product.image}
+									alt={mappedCartItem.product.title}
+									className="Cart-product-img"
+								/>
+							</Link>
 						</div>
 						<div className="col-md-4">
-							<p className="mb-1">{mappedCartItem.product.title}</p>
+							<p className="mb-1">
+								<Link to={`/products/${mappedCartItem.product.id}`}>
+									{mappedCartItem.product.title}
+								</Link>
+							</p>
 						</div>
 						<div className="col-md-2 mb-2">
 							<p className="carrot font-weight-bold mb-1">{`$${mappedCartItem.product.price}`}</p>
