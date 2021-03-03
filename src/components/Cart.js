5d6e0485037517070101d4522453e1e55b9dfe96
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-03 13:16:25 +0800

    Add a text if there is no item in the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,50 +8,54 @@ function Cart({ mappedCartItems, onQuantityInputChange, onCartItemDelete }) {
 	return (
 		<section className="row justify-content-center">
 			<div className="col-sm-10">
-				{mappedCartItems.map((mappedCartItem) => (
-					<div
-						key={mappedCartItem.product.id}
-						className="row align-items-center text-center mb-4"
-					>
-						<div className="col-md-3 mb-2">
-							<Link to={`/products/${mappedCartItem.product.id}`}>
-								<img
-									src={mappedCartItem.product.image}
-									alt={mappedCartItem.product.title}
-									className="Cart-product-img"
-								/>
-							</Link>
-						</div>
-						<div className="col-md-4">
-							<p className="mb-1">
+				{mappedCartItems.length < 1 ? (
+					<h1 className="text-center">You have no items in your cart</h1>
+				) : (
+					mappedCartItems.map((mappedCartItem) => (
+						<div
+							key={mappedCartItem.product.id}
+							className="row align-items-center text-center mb-4"
+						>
+							<div className="col-md-3 mb-2">
 								<Link to={`/products/${mappedCartItem.product.id}`}>
-									{mappedCartItem.product.title}
+									<img
+										src={mappedCartItem.product.image}
+										alt={mappedCartItem.product.title}
+										className="Cart-product-img"
+									/>
 								</Link>
-							</p>
+							</div>
+							<div className="col-md-4">
+								<p className="mb-1">
+									<Link to={`/products/${mappedCartItem.product.id}`}>
+										{mappedCartItem.product.title}
+									</Link>
+								</p>
+							</div>
+							<div className="col-md-2 mb-2">
+								<p className="carrot font-weight-bold mb-1">{`$${mappedCartItem.product.price}`}</p>
+								<button
+									type="button"
+									className="btn btn-danger"
+									onClick={(e) => onCartItemDelete(mappedCartItem.product.id)}
+								>
+									<FontAwesomeIcon icon={faTrash} />
+								</button>
+							</div>
+							<div className="col-md-3 d-flex justify-content-center">
+								<QuantityGroup
+									value={mappedCartItem.quantity}
+									onInputChange={(e) =>
+										onQuantityInputChange(
+											mappedCartItem.product.id,
+											parseInt(e.target.value, 10)
+										)
+									}
+								/>
+							</div>
 						</div>
-						<div className="col-md-2 mb-2">
-							<p className="carrot font-weight-bold mb-1">{`$${mappedCartItem.product.price}`}</p>
-							<button
-								type="button"
-								className="btn btn-danger"
-								onClick={(e) => onCartItemDelete(mappedCartItem.product.id)}
-							>
-								<FontAwesomeIcon icon={faTrash} />
-							</button>
-						</div>
-						<div className="col-md-3 d-flex justify-content-center">
-							<QuantityGroup
-								value={mappedCartItem.quantity}
-								onInputChange={(e) =>
-									onQuantityInputChange(
-										mappedCartItem.product.id,
-										parseInt(e.target.value, 10)
-									)
-								}
-							/>
-						</div>
-					</div>
-				))}
+					))
+				)}
 			</div>
 		</section>
 	);
