b7d70ce05587b667a211a2d41293d36f747087be
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-03 16:30:43 +0800

    Add and use Bootswatch (united theme)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootswatch/dist/united/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import App from './App';
